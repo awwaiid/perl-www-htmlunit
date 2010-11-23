f435797d64a7f00a3db3cad1e148c965c1111669
awwaiid <awwaiid@thelackthereof.org>
2010-11-22 22:21:29 -0500

    Add docs and clean some code for Sweetness
    Ignore-this: 4e69301ac56ddf839b8233eb4fe0e38
    
    darcs-hash:20101123032129-4fc8e-20c3ca8a6b921b926f916ee1cc1fc5821c57e914

--- lib/WWW/HtmlUnit/Sweet.pm
+++ lib/WWW/HtmlUnit/Sweet.pm
@@ -26,16 +26,17 @@ way you can treat it a lot more like a L<WWW::Mechanize> object.
 
 This module might change drastically, buyer beware!
 
+=head1 METHODS
+
 =cut
 
 use strict;
 use warnings;
 
-use WWW::HtmlUnit;
-
 use UNIVERSAL qw/isa can/;
 
 # This will be removed once WWW:HtmlUnit supports quiet-mode
+# Also... I think maybe it doesn't really work!
 my $saved_stderr;
 our $error_output;
 BEGIN {
@@ -53,18 +54,51 @@ BEGIN {
 # It would normally be just:
 # use WWW::HtmlUnit;
 
+=head2 $agent = WWW::HtmlUnit::Sweet->new
+
+Create a new sweet agent. Use this kinda like looking at a browser on the
+screen. The methods you call will be invoked (if possible) on the current
+browser, window, page, or focused element.
+
+The 'new' method can also take a browser version and a starting url, like this:
+
+  my $agent = WWW::HtmlUnit::Sweet->new(
+    version => 'FIREFOX_3',
+    url => 'http://google.com/'
+  );
+
+=cut
+
 sub new {
 	my $class = shift;
 	my $self = { @_ };
 	bless $self, $class;
-	$self->{browser} = WWW::HtmlUnit->new;
+	$self->{browser} = WWW::HtmlUnit->new( $self->{version} );
+  $self->getPage( $self->{url} ) if $self->{url};
 	return $self;
 }
 
-our $default_timeout = 30;
+=head2 $agent->wait_for(sub { ... }, $timeout)
 
-sub wait_for(&@) {
-  my ($subref, $timeout) = @_;
+Execute the provided sub once a second until it returns true, or until the the
+timeout has been reached. If a timeout isn't passed, it will default to 10
+seconds (which you can change by setting C<<
+$WWW::HtmlUnit::Sweet::default_timeout >>). This is handy for waiting for the
+page to finish executing some javascript, or loading.
+
+Example:
+
+  # Wait for an element with id 'foo' to exist
+  $agent->wait_for(sub {
+    $agent->getElementById('foo')
+  });
+
+=cut
+
+our $default_timeout = 10;
+
+sub wait_for {
+  my ($agent, $subref, $timeout) = @_;
   $timeout ||= $default_timeout;
   while($timeout) {
     return if eval { $subref->() };
@@ -74,6 +108,34 @@ sub wait_for(&@) {
   die "Timeout!\n";
 }
 
+=head2 AUTOLOAD, aka $agent->whatever(..)
+
+This is where the sweetness starts kicking in. First it will try to call
+->whatever on the browser, and if there is no method named 'whatever' there it
+will be called on the current window, and if there is no method named
+'whatever' there it will be called on the current page in that window, and if
+there is no method 'whatever' there it will be called on the currently focused
+element.
+
+Examples:
+
+  # This works at the browser level
+  $agent->getPage('http://google.com/');
+
+  # Get the 'name' for the current window
+  my $window_name = $agent->getName;
+
+  # Working from the current page, get an element by ID
+  my $sidebar_element = $agent->getElementById('sidebar');
+
+  # Click on the currently focused element
+  $agent->click;
+
+This scheme works quite well because HtmlUnit itself just so happens to not
+overlap their method names between different classes. Lucky us!
+
+=cut
+
 # This will make us act a bit more like Mechanize
 sub AUTOLOAD {
 	my $self = shift;
@@ -81,12 +143,19 @@ sub AUTOLOAD {
 	my $method = $AUTOLOAD; $method =~ s/.*:://;
 	return if $method eq 'DESTROY';
 	my $retval = eval {
-		if($self->{browser}->can($method)) {
-			return $self->{browser}->$method(@_);
-		} elsif($self->{browser}->getCurrentWindow->can($method)) {
-			return $self->{browser}->getCurrentWindow->$method(@_);
-		} elsif($self->{browser}->getCurrentWindow->getEnclosedPage->can($method)) {
-			return $self->{browser}->getCurrentWindow->getEnclosedPage->$method(@_);
+    my $browser = $self->{browser};
+    my $window = $browser && $browser->getCurrentWindow;
+    my $page = $window && $window->getEnclosedPage;
+    my $element = $page && $page->getFocusedElement;
+
+		if($browser && $browser->can($method)) {
+			return $browser->$method(@_);
+		} elsif($window && $window->can($method)) {
+			return $window->$method(@_);
+		} elsif($page && $page->can($method)) {
+			return $page->$method(@_);
+		} elsif($element && $element->can($method)) {
+			return $element->$method(@_);
 		} else {
 			die "Method $method not found!";
 		}
@@ -100,18 +169,17 @@ sub AUTOLOAD {
 	return $retval;
 }
 
-=head2 $agent->type
+=head1 TODO
 
-(Convenience function) Type into the currently focused element.
+Add more documentation and examples :)
 
 =cut
 
-sub type {
-	my $self = shift;
-	return $self->getFocusedElement->type(@_);
-}
 
 package WWW::HtmlUnit::com::gargoylesoftware::htmlunit::html::HtmlSelect;
+
+# Fix the get_option to take nicer params
+# TODO: document this!
 
 sub get_option {
 	my ($self, %params) = @_;
@@ -122,6 +190,22 @@ sub get_option {
 	}
 	die "Must pass either text or value";
 }
+
+=head1 SEE ALSO
+
+L<WWW::HtmlUnit>
+
+=head1 AUTHOR
+
+  Brock Wilcox <awwaiid@thelackthereof.org> - http://thelackthereof.org/
+
+=head1 COPYRIGHT
+
+  Copyright (c) 2009-2010 Brock Wilcox <awwaiid@thelackthereof.org>. All rights
+  reserved.  This program is free software; you can redistribute it and/or
+  modify it under the same terms as Perl itself.
+
+=cut
 
 1;
 
